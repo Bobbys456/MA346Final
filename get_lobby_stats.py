72872dac90644b9c8e4d928774d3f93b7dc105cf
Bobby Sullivan <bobbysullivan61@gmail.com>
2023-12-07 00:07:45 -0500

    everything working better lobbyign stuff implemented only need to fix the bill text finder it stops at like 5 % now

--- get_lobby_stats.py
+++ get_lobby_stats.py
@@ -1,13 +1,23 @@
 import requests as req
 from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
+
+
+years = {}
 
 def main(): 
     for date in range(1990, 2023, 2):
         response = req.get('https://www.opensecrets.org/industries/summary.php?ind=E&recipdetail=S&sortorder=A&mem=Y&cycle='+str(date))
         if response.status_code == 200: 
-            get_table_data(html = response.text)
+            years[date] = get_table_data(html = response.text, date=date)
 
-def get_table_data(html): 
+    pd.DataFrame(years).to_csv(os.path.join('data', 'Senate_lobby_totals_by_year.csv'))
+
+def get_table_data(html,date): 
+    
+    values = {}
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(html, "html.parser")
 
@@ -15,11 +25,33 @@ def get_table_data(html):
     table = soup.find('table', {'class': 'datadisplay'})
     # Iterate through the rows of the table
     for row in table.find_all('tr'):
-        # Iterate through the cells of each row
-        for cell in row.find_all('td'):
-            # Extract and process the data from each cell
-            data = cell.get_text()
-            print(data + 'dfdsfsdf\n\n\n\n\n')
 
+        negative = False
+        # Iterate through the cells of each row
+
+        cells = row.find_all('td')
+
+        if len(cells) > 0: 
+
+            name = str(cells[0].get_text())
+
+            #store number in varibale and check if it is negative
+            num = str(cells[1].get_text())
+            if '-' in num: 
+                negative = True
+            
+            #eliminate non numeric characters
+            num = re.sub("[^0-9]", "", num)
+            
+            #account for numbers being negative since negative sign is stripped earlier
+            if negative: 
+                num = int(num) * -1
+
+            values[name] = num
+
+    return values
+        
+     
+    
 
 main() 
